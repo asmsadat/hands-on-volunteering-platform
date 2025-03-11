75cb89d828b1a0b09b519ec9aa930ab16b21ec52
asmsadat <u1804105@student.cuet.ac.bd>
2025-03-11 16:36:18 +0600

    feat: add auth

---
+++ backend/controller/authController.js
@@ -0,0 +1,44 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/userModel");
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await UserModel.findByEmail(email);
+    if (existingUser)
+      return res.status(400).json({ error: "Email already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await UserModel.create({ name, email, hashedPassword });
+
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user: newUser });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findByEmail(email);
+    if (!user) return res.status(400).json({ error: "Invalid email" });
+
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.status(400).json({ error: "Invalid password" });
+
+    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ message: "Authentication successful", token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { signup, signin };
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
+
 const eventRoutes = require("./routes/eventRoutes");
 const helpRoutes = require("./routes/helpRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 app.use(cors());
@@ -13,5 +15,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/events", eventRoutes);
 app.use("/api/helps", helpRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
